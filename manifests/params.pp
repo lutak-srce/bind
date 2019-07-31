afe59a3ddb847c046db8d56dd7f0ab0d54fbf3ba
Ante Jurjević <ante.jurjevic@srce.hr>
2019-07-31 15:13:04 +0200

    Initial commit

---
+++ manifests/params.pp
@@ -0,0 +1,35 @@
+# Class: bind::params
+#
+class bind::params {
+
+  case $::osfamily {
+    'RedHat': {
+      $packagenameprefix = 'bind'
+      $servicename       = 'named'
+      $binduser          = 'root'
+      $bindgroup         = 'named'
+      $bindlogdir        = '/var/log/named'
+    }
+    'Debian': {
+      $packagenameprefix = 'bind9'
+      $servicename       = 'bind9'
+      $binduser          = 'bind'
+      $bindgroup         = 'bind'
+      $bindlogdir        = '/var/log/named'
+    }
+    'Freebsd': {
+      $packagenameprefix = 'bind910'
+      $servicename       = 'named'
+      $binduser          = 'bind'
+      $bindgroup         = 'bind'
+      $bindlogdir        = '/var/log/named'
+    }
+    default: {
+      $packagenameprefix = 'bind'
+      $servicename       = 'named'
+      $binduser          = 'root'
+      $bindgroup         = 'named'
+      $bindlogdir        = '/var/log/named'
+    }
+  }
+}
