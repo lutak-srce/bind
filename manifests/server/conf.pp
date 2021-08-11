afe10b3db1941d3fd95aa914e6359fbc4dfb8dd1
Ante Jurjevic <ante.jurjevic@srce.hr>
2021-08-11 13:05:33 +0200

    Add few attributes in init

--- manifests/server/conf.pp
+++ manifests/server/conf.pp
@@ -105,7 +105,7 @@ define bind::server::conf (
   $transfer_format        = undef,
   $max_transfer_time_in   = undef,
   $interface_interval     = undef,
-  $notify                 = undef,
+  $notify_                = undef,
   $logging                = {},
   $tcp_clients            = '100',
   $recursive_clients      = '1000',
